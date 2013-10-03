d8464fa70e464253e73be2ee7f6ecff9e07d5702
sandro <sandro>
2013-10-03 17:23:31 +0000

    GML parser: supporting <gml:Box> elements

--- src/gaiageo/gg_gml.c
+++ src/gaiageo/gg_gml.c
@@ -85,6 +85,7 @@ the terms of any one of the MPL, the GPL or the LGPL.
 #define GAIA_GML_MULTIPOLYGON		8
 #define GAIA_GML_MULTISURFACE		9
 #define GAIA_GML_MULTIGEOMETRY		10
+#define GAIA_GML_BOX			11
 
 #define GML_DYN_NONE	0
 #define GML_DYN_DYNLINE	1
@@ -688,6 +689,20 @@ guessGmlSrid (gmlNodePtr node)
 			      }
 			}
 		  }
+		if (len > 40)
+		  {
+		      if (strncmp
+			  (attr->Value,
+			   "http://www.opengis.net/gml/srs/epsg.xml#", 40) == 0)
+			{
+			    int i = strlen (attr->Value) - 1;
+			    for (; i >= 0; i--)
+			      {
+				  if (*(attr->Value + i) == '#')
+				      return atoi (attr->Value + i + 1);
+			      }
+			}
+		  }
 	    }
 	  attr = attr->Next;
       }
@@ -748,6 +763,9 @@ guessGmlGeometryType (gmlNodePtr node)
     if (strcmp (node->Tag, "gml:MultiGeometry") == 0
 	|| strcmp (node->Tag, "MultiGeometry") == 0)
 	type = GAIA_GML_MULTIGEOMETRY;
+    if (strcmp (node->Tag, "gml:Box") == 0
+	|| strcmp (node->Tag, "Box") == 0)
+	type = GAIA_GML_BOX;
     return type;
 }
 
@@ -1279,6 +1297,103 @@ gml_count_dyn_points (gaiaDynamicLinePtr dyn)
 	  pt = pt->Next;
       }
     return iv;
+}
+
+static int
+gml_parse_box (struct gml_data *p_data, gaiaGeomCollPtr geom,
+		      gmlNodePtr node, int srid, gmlNodePtr * next)
+{
+/* parsing a <gml:Box> */
+    gaiaGeomCollPtr last;
+    gaiaGeomCollPtr pg;
+    gaiaPolygonPtr new_pg;
+    gaiaRingPtr ring;
+    gaiaPointPtr pt;
+double minx;
+double miny;
+double maxx;
+double maxy;
+int has_z;
+    int points = 0;
+    gaiaDynamicLinePtr dyn = gaiaAllocDynamicLine ();
+    gmlMapDynAlloc (p_data, GML_DYN_DYNLINE, dyn);
+
+    if (strcmp (node->Tag, "gml:coordinates") == 0
+	|| strcmp (node->Tag, "coordinates") == 0)
+      {
+	  /* parsing a GML v.2.x <gml:LineString> */
+	  if (!gml_parse_coordinates (node->Coordinates, dyn, &has_z))
+	      goto error;
+	  node = node->Next;
+	  if (node == NULL)
+	      goto error;
+	  if (strcmp (node->Tag, "gml:coordinates") == 0
+	      || strcmp (node->Tag, "coordinates") == 0)
+	      ;
+	  else
+	      goto error;
+	  node = node->Next;
+	  if (node == NULL)
+	      goto error;
+	  if (strcmp (node->Tag, "gml:Box") == 0
+	      || strcmp (node->Tag, "Box") == 0)
+	      ;
+	  else
+	      goto error;
+	  *next = node->Next;
+	  goto ok;
+      }
+
+  ok:
+/* ok, GML nodes match as expected */
+    points = gml_count_dyn_points (dyn);
+    if (points != 2)
+	goto error;
+	  pt = dyn->First;
+minx = pt->X;
+miny = pt->Y;
+maxx = pt->X;
+maxy = pt->Y;
+	  while (pt)
+	    {
+		if (pt->X < minx)
+minx = pt->X;
+if (pt->Y < miny)
+miny = pt->Y;
+if (pt->X > maxx)
+maxx = pt->X;
+if (pt->Y > maxy)
+maxy = pt->Y;
+		pt = pt->Next;
+	    }
+pg = gaiaAllocGeomColl ();
+	  gmlMapDynAlloc (p_data, GML_DYN_GEOM, pg);
+	  pg->Srid = srid;
+	  new_pg = gaiaAddPolygonToGeomColl (pg, 5, 0);
+	  /* initializing the EXTERIOR RING */
+	  ring = new_pg->Exterior;
+		gaiaSetPoint (ring->Coords, 0, minx, miny);
+		gaiaSetPoint (ring->Coords, 1, maxx, miny);
+		gaiaSetPoint (ring->Coords, 2, maxx, maxy);
+		gaiaSetPoint (ring->Coords, 3, minx, maxy);
+		gaiaSetPoint (ring->Coords, 4, minx, miny);
+    last = geom;
+    while (1)
+      {
+	  /* searching the last Geometry within chain */
+	  if (last->Next == NULL)
+	      break;
+	  last = last->Next;
+      }
+    last->Next = pg;
+    gmlMapDynClean (p_data, dyn);
+    gaiaFreeDynamicLine (dyn);
+    return 1;
+
+  error:
+    gmlMapDynClean (p_data, dyn);
+    gaiaFreeDynamicLine (dyn);
+    return 0;
 }
 
 static int
@@ -3285,6 +3400,12 @@ gml_build_geometry (struct gml_data *p_data, gmlNodePtr tree,
       case GAIA_GML_MULTIGEOMETRY:
 	  geom->DeclaredType = GAIA_GEOMETRYCOLLECTION;
 	  if (!gml_parse_multi_geometry (p_data, geom, tree->Next))
+	      goto error;
+	  break;
+      case GAIA_GML_BOX:
+	  geom->DeclaredType = GAIA_POLYGON;
+	  if (!gml_parse_box
+	      (p_data, geom, tree->Next, geom->Srid, &next))
 	      goto error;
 	  break;
       };
