6ab3821b05d347771531a585ce7494796cdc6b50
sandro <sandro>
2014-11-22 11:36:00 +0000

    implementing the eval() SQL function

--- src/spatialite/spatialite.c
+++ src/spatialite/spatialite.c
@@ -173,6 +173,131 @@ struct gpkg_table
     struct gpkg_table *next;
 };
 
+
+/*
+************************************************************************
+**
+** the following code has been integrally copied from SQLite's own sources:
+** -/ext/misc/eval.c
+*/
+
+/*
+** 2014-11-10
+**
+** The author disclaims copyright to this source code.  In place of
+** a legal notice, here is a blessing:
+**
+**    May you do good and not evil.
+**    May you find forgiveness for yourself and forgive others.
+**    May you share freely, never taking more than you give.
+**
+******************************************************************************
+**
+** This SQLite extension implements SQL function eval() which runs
+** SQL statements recursively.
+*/
+
+struct EvalResult
+{
+/*
+** Structure used to accumulate the output
+*/
+    char *z;			/* Accumulated output */
+    const char *zSep;		/* Separator */
+    int szSep;			/* Size of the separator string */
+    unsigned int nAlloc;	/* Number of bytes allocated for z[] */
+    int nUsed;			/* Number of bytes of z[] actually used */
+};
+
+static int
+eval_callback (void *pCtx, int argc, char **argv, char **colnames)
+{
+/*
+** Callback from sqlite_exec() for the eval() function.
+*/
+    struct EvalResult *p = (struct EvalResult *) pCtx;
+    int i;
+
+    if (colnames == NULL)
+	colnames = NULL;	/* silencing stupid compiler warnings */
+
+    for (i = 0; i < argc; i++)
+      {
+	  const char *z = argv[i] ? argv[i] : "";
+	  size_t sz = strlen (z);
+	  if (sz + p->nUsed + p->szSep + 1 > p->nAlloc)
+	    {
+		char *zNew;
+		p->nAlloc = p->nAlloc * 2 + sz + p->szSep + 1;
+		zNew = sqlite3_realloc (p->z, p->nAlloc);
+		if (zNew == 0)
+		  {
+		      sqlite3_free (p->z);
+		      memset (p, 0, sizeof (*p));
+		      return 1;
+		  }
+		p->z = zNew;
+	    }
+	  if (p->nUsed > 0)
+	    {
+		memcpy (&p->z[p->nUsed], p->zSep, p->szSep);
+		p->nUsed += p->szSep;
+	    }
+	  memcpy (&p->z[p->nUsed], z, sz);
+	  p->nUsed += sz;
+      }
+    return 0;
+}
+
+static void
+fnct_EvalFunc (sqlite3_context * context, int argc, sqlite3_value ** argv)
+{
+/*
+** Implementation of the eval(X) and eval(X,Y) SQL functions.
+**
+** Evaluate the SQL text in X.  Return the results, using string
+** Y as the separator.  If Y is omitted, use a single space character.
+*/
+    const char *zSql;
+    sqlite3 *db;
+    char *zErr = 0;
+    int rc;
+    struct EvalResult x;
+
+    memset (&x, 0, sizeof (x));
+    x.zSep = " ";
+    zSql = (const char *) sqlite3_value_text (argv[0]);
+    if (zSql == 0)
+	return;
+    if (argc > 1)
+      {
+	  x.zSep = (const char *) sqlite3_value_text (argv[1]);
+	  if (x.zSep == 0)
+	      return;
+      }
+    x.szSep = (int) strlen (x.zSep);
+    db = sqlite3_context_db_handle (context);
+    rc = sqlite3_exec (db, zSql, eval_callback, &x, &zErr);
+    if (rc != SQLITE_OK)
+      {
+	  sqlite3_result_error (context, zErr, -1);
+	  sqlite3_free (zErr);
+      }
+    else if (x.zSep == 0)
+      {
+	  sqlite3_result_error_nomem (context);
+	  sqlite3_free (x.z);
+      }
+    else
+      {
+	  sqlite3_result_text (context, x.z, x.nUsed, sqlite3_free);
+      }
+}
+
+/* 
+******** end -/ext/misc/eval.c *****
+*/
+
 static void
 fnct_spatialite_version (sqlite3_context * context, int argc,
 			 sqlite3_value ** argv)
@@ -18157,7 +18282,8 @@ fnct_Buffer (sqlite3_context * context, int argc, sqlite3_value ** argv)
       {
 	  void *data = sqlite3_user_data (context);
 	  if (data != NULL)
-	      result = gaiaGeomCollBuffer_r (data, geo, radius, quadrantsegments);
+	      result =
+		  gaiaGeomCollBuffer_r (data, geo, radius, quadrantsegments);
 	  else
 	      result = gaiaGeomCollBuffer (geo, radius, quadrantsegments);
 	  if (!result)
@@ -30244,6 +30370,11 @@ register_spatialite_sql_functions (void *p_db, const void *p_cache)
     sqlite3_create_function_v2 (db, "CloneTable", 14,
 				SQLITE_UTF8 | SQLITE_DETERMINISTIC, 0,
 				fnct_CloneTable, 0, 0, 0);
+
+    sqlite3_create_function_v2 (db, "eval", 1, SQLITE_UTF8, 0, fnct_EvalFunc, 0,
+				0, 0);
+    sqlite3_create_function_v2 (db, "eval", 2, SQLITE_UTF8, 0, fnct_EvalFunc, 0,
+				0, 0);
 
 #ifndef OMIT_PROJ		/* PROJ.4 is strictly required to support KML */
     sqlite3_create_function_v2 (db, "AsKml", 1,
