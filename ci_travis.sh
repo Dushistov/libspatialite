f113c4bfc8441968bba91e901a1943ba512d5ed4
Evgeniy A. Dushistov <dushistov@mail.ru>
2018-01-06 11:56:57 +0300

    also check cmake build in travis

---
+++ ci_travis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+TCI_NUMTHREADS=2
+if [[ -f /sys/devices/system/cpu/online ]]; then
+	# Calculates 1.5 times physical threads
+	TCI_NUMTHREADS=$(( ( $(cut -f 2 -d '-' /sys/devices/system/cpu/online) + 1 ) * 15 / 10  ))
+fi
+
+run_make()
+{
+    [ $TCI_NUMTHREADS -gt 0 ] && make -j $TCI_NUMTHREADS || make 
+}
+
+if [ "$LIBSPATIALITE_BUILD_TOOL" == "autotools" ]; then
+    ./configure --prefix=/usr --enable-libxml2 --disable-freexl
+    run_make
+    make check
+else
+    rm -fr travis_cmake_build
+    mkdir travis_cmake_build
+    cd travis_cmake_build
+    cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+    run_make
+    ctest -V --output-on-failure
+fi
